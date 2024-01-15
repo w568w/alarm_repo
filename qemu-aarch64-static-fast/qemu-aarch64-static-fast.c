5d1fae1544477469c8d2e05a1c9ab321299fa23a
w568w <1278297578@qq.com>
2024-01-15 11:31:30 +0800

    fix: duplicate parameters unprocessed in qemu wrapper

--- qemu-aarch64-static-fast/qemu-aarch64-static-fast.c
+++ qemu-aarch64-static-fast/qemu-aarch64-static-fast.c
@@ -1,18 +1,22 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 int main(int ac, char* av[], char* envp[]) {
-    char** new_av = calloc(ac + 2 + 1, sizeof(char *));
+    char** new_av = calloc(ac + 1 + 1, sizeof(char *));
     if (!new_av) {
         exit(EXIT_FAILURE);
     }
+    
     new_av[0] = av[0];
     new_av[1] = strdup("-cpu");
     new_av[2] = strdup("max,pauth-impdef=on");
-    for (int i = 1; i < ac; ++i) {
-        new_av[i+2] = av[i];
+    new_av[3] = av[1];
+    // skip av[2], which is the executable name
+    for (int i = 3; i < ac; ++i) {
+        new_av[i+1] = av[i];
     }
-    new_av[ac + 2] = NULL;
-    execve("/usr/bin/qemu-aarch64-static", new_av, envp);
+    new_av[ac + 1] = NULL;
+    return execve("/usr/bin/qemu-aarch64-static", new_av, envp);
 }
